e2ed7aec517b44c8c40c83084fb2f249826256c7
Emtiaz-Rahman <emtiazrahman002@gmail.com>
2022-03-22 21:13:11 +0600

    set set eventhandler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product)
+
     const { name, img, price, seller, ratings } = props.product
+    
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='product-info'>
-            <p className='product-name'>{name}</p>
-            <p>Price:${price}</p>
-            <p>Seller:{seller}</p>
-            <p>Ratings:{ratings} start</p>
+                <p className='product-name'>{name}</p>
+                <p>Price:${price}</p>
+                <p>Seller:{seller}</p>
+                <p>Ratings:{ratings} start</p>
             </div>
-            <button className='btn-cart'>
+            <button onClick={()=>props.handleAddToCart(props.product)} className='btn-cart'>
                 <p>Add to Cart</p>
             </button>
         </div>
